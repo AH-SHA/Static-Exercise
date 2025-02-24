a20c54bcbc5b58b046284c7253b9483d06345753
AH-SHA <email.sa.coder@gmail.com>
2025-02-24 13:29:28 -0500

    Initial Commit - GitHub Workflow not cooperating
    Manually adding updated repository and files because VSS nor command line would properly update GitHub.

---
+++ Static-Exercise/StaticExercise/FahrenheitToCelsius.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    //1. Create a static class and name it
+    public static class TempConverter
+    {
+        
+        //Notice, there is no empty static constructor used at this point, because it is not needed.  The Class only contains methods.
+        //NOT NEEDED:  "static class TempConverter(){}" - statement not needed in this class, but some classes with fields and properties contain it. Not those only using methods.
+
+        //Creating 2 methods needed to calculate tempurature conversions (lsited below)
+
+        //Ensure 'static' is used in Method's signature because all members of a static class must be static
+        //The method's parameter lists the type of variable and the variable name. That variable is created in the Program file's Main Program method
+        //The method's calculation's result is stored into a new variable 'celsius Answer'
+        //A "return" statement is used in the Method, as all method's require.
+        //The Method's value is returned to a variable. The same variable in the Main Program file that is equal to the Call of the Method
+        public static double ConvertFahrenToCel(double userFahren)
+        {
+            //Note, the use of '0.555555556' reflects the decimal version of '5/9'
+            /*Because we have listed the 'double" as input and output for the method,
+            C# reads a fraction as two integers, so it won't compute the formula without you first converting the fraction into a decimal*/
+            //Original conversion formula is: ((fahrenheit temprature - 32) * 5/9)
+
+
+            var celsiusAnswer = (((userFahren) - 32) * 0.55555555556);
+            return celsiusAnswer;
+            
+
+        }
+
+        public static double ConvertCelsiusToFahren(double userCelsius)
+        {
+            
+            var fahrenAnswer = (((userCelsius) * 1.8) + 32);
+
+            return fahrenAnswer;
+
+            
+        }
+       
+    }
+}
---
+++ Static-Exercise/StaticExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace StaticExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fahrenheit to Celsius conversion calculator
+            //1. Get user input
+            Console.WriteLine("");
+            Console.WriteLine("*******Celsius Conversion*******");
+            Console.WriteLine("Enter Fahrenheit temperature:");
+
+            //2. Convert user's input from string into 'double' format, so it works as a parameter in the Method's calculations
+            double userFahren = double.Parse(Console.ReadLine());
+
+            //3. Call the static method created in the class,by using the "Class'sType.StaticProperty = value " syntax
+            //The Method was created within a Class (static Class) in the Project File
+
+            //4. Store the Method's result in a variable also used in the method.
+            //The variable used in the Method's parameter, was created from the user's input. It also used in the Method's scope.
+            var celsiusAnswer = TempConverter.ConvertFahrenToCel(userFahren);
+
+            //5. Print the answer to the console
+            Console.WriteLine($"{celsiusAnswer} degrees Celsius");
+
+
+            //Calculator to convert Celsius to Fahrenheit. Same steps used below as above.
+            Console.WriteLine("");
+            Console.WriteLine("*******Fahrenheit Conversion******");
+            Console.WriteLine("Enter Celsisus temperature:");
+            double userCelsius = double.Parse(Console.ReadLine());
+
+            var fahrenAnswer = TempConverter.ConvertCelsiusToFahren(userCelsius);
+
+            Console.WriteLine($"{fahrenAnswer} degrees Fahrenheit");
+
+        }
+
+        
+
+    }
+}
